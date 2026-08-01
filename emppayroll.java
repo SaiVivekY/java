34a72a893695b970b096b44430e169b2eca1df1e
Sai Vivek Yekkanti <sai.yekkanti.btech2025@sitpune.edu.in>
2026-08-01 18:59:22 +0530

    Add files via upload

---
+++ emppayroll.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class emppayroll {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the emplyee id: ");
+        String id = scanner.nextLine();
+        System.out.println("Enter the base salary: ");
+        String sal = scanner.nextLine();
+        System.out.println("ENter the bonus amount");
+        String bonus = scanner.nextLine();
+
+        int intsal = Integer.parseInt(sal);
+        int intbon = Integer.parseInt(bonus);
+
+        int netsal = intsal + intbon;
+        System.out.println("the total salary of the employee: " + id + "is : " + netsal);
+
+    }
+}
