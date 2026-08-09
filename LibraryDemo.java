48984cb5ebaddadf8c806a6d33f04ed0a1422fb0
Sai Vivek Yekkanti <sai.yekkanti.btech2025@sitpune.edu.in>
2026-08-09 21:16:24 +0530

    Add files via upload

---
+++ LibraryDemo.java
@@ -0,0 +1,11 @@
+import library.Book;
+
+public class LibraryDemo {
+
+    public static void main(String[] args) {
+
+        Book b = new Book(1, "The Great Gatsby", "F. Scott Fitzgerald", 299.99);
+
+        b.display();
+    }
+}
