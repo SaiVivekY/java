fb1364d5005247bc55db07a9d7a04396d64ab7df
Sai Vivek Yekkanti <sai.yekkanti.btech2025@sitpune.edu.in>
2026-08-29 23:32:17 +0530

    Add files via upload

---
+++ DrivingLicense.java
@@ -0,0 +1,36 @@
+package exp16;
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class DrivingLicense {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+
+            if (age < 18) {
+                throw new InvalidAgeException(
+                    "You are not eligible for a driving license"
+                );
+            }
+
+            System.out.println(
+                "You are eligible for a driving license"
+            );
+
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
